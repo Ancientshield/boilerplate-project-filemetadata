452b475073621c49df0dfdbf5e2faab1ca1c68ad
Ancientshield <jacklin2100@gmail.com>
2023-12-11 00:16:01 +0800

    finished File Metadata Microservice

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+// use multer
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// API
+app.get('/', (req, res) => {
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+	const { originalname, mimetype, size } = req.file;
+	res.json({
+		name: originalname,
+		type: mimetype,
+		size: size,
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log('Your app is listening on port ' + port);
 });
